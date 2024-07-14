4c963a1757232793f12f8a514b41e8d323d26048
Axl <redotola117@gmail.com>
2024-07-14 17:40:30 -0600

    Manejo de conversiones de datos

--- hola.go
+++ hola.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
-    "rsc.io/quote"
 	"math"
+	"strconv"
+
+	"rsc.io/quote"
 )
 
 // Declaraciones de constantes
@@ -30,54 +32,105 @@ const (
 
 func main() {
 
-	// Imprimir el paquete de Go
-	fmt.Println("Hola Mundo")
-	fmt.Println(quote.Go())
+	manejo_de_datos()
+}
 
-	// Declaracion de variables
-	var firstName, lastName string
-	var age int
+func imprimir_datos(){
 
-	// Otra forma de declarar variables
-	var (
-		firstName1, lastName1 	string = "Axl", "Castrejón"
-		age1 					int	= 21
-	)
+		// Imprimir el paquete de Go
+		fmt.Println("Hola Mundo")
+		fmt.Println(quote.Go())
+	
+		// Declaracion de variables
+		var firstName, lastName string
+		var age int
+	
+		// Otra forma de declarar variables
+		var (
+			firstName1, lastName1 	string = "Axl", "Castrejón"
+			age1 					int	= 21
+		)
+	
+		// Otra forma de declarar
+		var (
+			firstName2, lastName2, age2 = "Axl", "Castrejón", 21 // Inicializando variables en la misma línea
+		)
+	
+		// Declarar variable nueva sin usar var, solo dentro de las funciones
+		firstName3, lastName3 := "Axl", "Castrejón" 
+		age3 :=  21
+	
+		firstName = "Axl"
+		lastName = "Castrejón"
+		age = 21
+	
+		fmt.Println(firstName, lastName, age)
+		fmt.Println(firstName1, lastName1, age1)
+		fmt.Println(firstName2, lastName2, age2)
+		fmt.Println(firstName3, lastName3, age3)
+		fmt.Println(Pi)
+		fmt.Println(x, y, z, w)
+	
+		/*Tipos de datos*/
+	
+		// Numericos
+		// Cuando la variable se declara "int" el sistema decide el tamaño conforme la arquitectura
+		var integer int8 = 12
+	
+		// Uint solo almacena numeros enteros positivos
+		var integeruint uint = 3
+	
+		// Flotantes 
+		var float float64 = 3.34
+	
+		// El menor y mayor rango que se puede alcanzar con los tipos de datos
+		fmt.Println(math.MinInt64, math.MaxInt64)
+		fmt.Println(integer, integeruint, float)
+		fmt.Println(math.MaxFloat32, math.MaxUint32)
+		
+		// Formateo de valores en string
+		fullName := "Alex Roel \t(alias \"roelcode\")\n"
+	
+		// Booleanos
+		var valueBool bool = false
+	
+		// Bits
+		var a byte = 'a'
+	
+		// Impresion de contenido
+		fmt.Println(fullName, valueBool, a)
+	
+		s := "hola"
+	
+		fmt.Println(s[0])
+	
+		// Running
+	
+		var r rune = '🖤'
+		fmt.Println(r)
+}
 
-	// Otra forma de declarar
-	var (
-		firstName2, lastName2, age2 = "Axl", "Castrejón", 21 // Inicializando variables en la misma línea
-	)
 
-	// Declarar variable nueva sin usar var, solo dentro de las funciones
-	firstName3, lastName3 := "Axl", "Castrejón" 
-	age3 :=  21
+func manejo_de_datos(){
 
-	firstName = "Axl"
-	lastName = "Castrejón"
-	age = 21
+	// Manejo de conversiones de datos para operaciones
+	var integer16 int16 = 50
+	var integer32 int32 = 100
 
-	fmt.Println(firstName, lastName, age)
-	fmt.Println(firstName1, lastName1, age1)
-	fmt.Println(firstName2, lastName2, age2)
-	fmt.Println(firstName3, lastName3, age3)
-	fmt.Println(Pi)
-	fmt.Println(x, y, z, w)
+	// Impresion de la suma
+	fmt.Println(integer16 + int16(integer32))
 
-	/*Tipos de datos*/
+	// Conversion de string a entero con error controlado
+	s := "100"
+	i, _ := strconv.Atoi(s)
+	fmt.Println("Impresion de  el valor sumado y convertido: ")
+	fmt.Println(i + 1)
 
-	// Numericos
-	// Cuando la variable se declara "int" el sistema decide el tamaño conforme la arquitectura
-	var integer int8 = 12
+	// Conversion de entero a strig
+	n := 42
+	s = strconv.Itoa(n)
+	fmt.Println("Conversion de entero a string: ", s)
+	fmt.Println("Suma de cadenas: ", s+s)
 
-	// Uint solo almacena numeros enteros positivos
-	var integeruint uint = 3
-
-	// Flotantes 
-	var float float64 = 3.34
-
-	// El menor y mayor rango que se puede alcanzar con los tipos de datos
-	fmt.Println(math.MinInt64, math.MaxInt64)
-	fmt.Println(integer, integeruint, float)
 
 }
